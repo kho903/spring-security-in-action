f6032bfbbfa944d6824140aad090406d5370a48c
Kim Ji Hun <gmldnr2222@naver.com>
2023-05-21 16:04:00 +0900

    feat: 2.3.2 엔드포인트 권한 부여 구성 재정의
    - WebSecurityConfigurerAdapter 클래스 확장 후 configure(HttpSecurity http) 메서드 재정의

--- ch2-ex1/src/main/java/com/jikim/security/config/ProjectConfig.java
+++ ch2-ex1/src/main/java/com/jikim/security/config/ProjectConfig.java
@@ -2,6 +2,8 @@ package com.jikim.security.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
@@ -9,7 +11,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
-public class ProjectConfig {
+public class ProjectConfig extends WebSecurityConfigurerAdapter {
 
 	@Bean
 	public UserDetailsService userDetailsService() {
@@ -26,5 +28,15 @@ public class ProjectConfig {
 	@Bean
 	public PasswordEncoder passwordEncoder() {
 		return NoOpPasswordEncoder.getInstance(); // PasswordEncoder 를 컨텍스트에 추가하기 위해 @Bean 어노테이션이 지정된 새 메서드
+	}
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.httpBasic()
+			.and()
+			.authorizeRequests()
+			.anyRequest()
+			.authenticated(); // 모든 요청에 인증이 필요.
+			// .permitAll(); // 인증 없이 요청 가능
 	}
 }
