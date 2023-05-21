f328bf4b5b2cc8058db809e2d05fbc21a96d935e
Kim Ji Hun <gmldnr2222@naver.com>
2023-05-21 17:02:08 +0900

    config: ch2-ex4, 2.3.5

---
+++ ch2-ex4/src/main/java/com/jikim/security/Ch2Ex4Application.java
@@ -0,0 +1,13 @@
+package com.jikim.security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ch2Ex4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ch2Ex4Application.class, args);
+	}
+
+}
