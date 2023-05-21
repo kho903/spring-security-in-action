9d0531971338dc857aa80da2c0355a1e5beb3585
Kim Ji Hun <gmldnr2222@naver.com>
2023-05-21 18:30:21 +0900

    conf: ch3-ex1, 3.3.2

---
+++ ch3-ex1/src/main/java/com/jikim/security/Ch3Ex1Application.java
@@ -0,0 +1,13 @@
+package com.jikim.security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ch3Ex1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ch3Ex1Application.class, args);
+	}
+
+}
---
+++ ch3-ex1/src/test/java/com/jikim/security/Ch3Ex1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jikim.security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ch3Ex1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
