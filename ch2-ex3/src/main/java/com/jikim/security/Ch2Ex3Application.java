abff8b8c8fef8476a54d1c66dc18eff6fb072071
Kim Ji Hun <gmldnr2222@naver.com>
2023-05-21 16:35:27 +0900

    conf: ch2-ex3, 2.3.4

---
+++ ch2-ex3/src/main/java/com/jikim/security/Ch2Ex3Application.java
@@ -0,0 +1,13 @@
+package com.jikim.security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ch2Ex3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ch2Ex3Application.class, args);
+	}
+
+}
