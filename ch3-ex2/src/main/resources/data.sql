fd8f4e31879d0bd852db7e565494501392bb523c
Kim Ji Hun <gmldnr2222@naver.com>
2023-05-23 14:57:11 +0900

    feat: add datasource setting

---
+++ ch3-ex2/src/main/resources/data.sql
@@ -0,0 +1,2 @@
+INSERT IGNORE INTO `security_inaction`.`authorities` VALUES (NULL, 'john', 'write');
+INSERT IGNORE INTO `security_inaction`.`users` VALUES (NULL, 'john', '12345', '1');
---
+++ ch3-ex2/src/main/resources/schema.sql
@@ -0,0 +1,16 @@
+
+
+CREATE TABLE IF NOT EXISTS `security_inaction`.`users` (
+`id` INT NOT NULL AUTO_INCREMENT,
+`username` VARCHAR(45) NOT NULL,
+`password` VARCHAR(45) NOT NULL,
+`enabled` INT NOT NULL,
+PRIMARY KEY (`id`)
+);
+
+CREATE TABLE IF NOT EXISTS `security_inaction`.`authorities` (
+    `id` INT NOT NULL AUTO_INCREMENT,
+    `username` VARCHAR(45) NOT NULL,
+    `authority` VARCHAR(45) NOT NULL,
+    PRIMARY KEY (`id`)
+);
